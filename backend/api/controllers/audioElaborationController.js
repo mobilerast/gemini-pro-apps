62a8d9af01cfb4266cd601bd33fc48eb35eeaa47
malp <mehmet.alp@rastmobile.com>
2024-04-01 22:51:05 +0100

    added: elaboratedAudio controller and geminiPro

--- backend/api/controllers/audioElaborationController.js
+++ backend/api/controllers/audioElaborationController.js
@@ -1,11 +1,16 @@
 const audioFileModel = require('../../models/audioFile');
 const geminiPro = require('../../integration/geminiPro');
+const fileUpload = require('../../utils/fileUpload');
 
 exports.uploadAudio = async (req, res) => {
   try {
+    const uploadedFile = await fileUpload.uploadFile(req);
 
-    const elaboratedAudio = await geminiPro.elaborateAudio(audioFile);
+    const elaboratedAudio = await geminiPro.elaborateAudio(uploadedFile);
 
+    console.log(elaboratedAudio);
+
+    res.json({ elaboratedAudioUrl: elaboratedAudioUrl });
   } catch (err) {
     console.log(err);
   }
