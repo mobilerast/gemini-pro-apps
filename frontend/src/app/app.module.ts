c46514e4591087f0d8ac49422991877ead6b7920
malp <mehmet.alp@rastmobile.com>
2024-04-01 22:46:45 +0100

    fix: HttpClientModule imported in app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FileEloborateComponent } from './file-eloborate/file-eloborate.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { FileEloborateComponent } from './file-eloborate/file-eloborate.componen
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
